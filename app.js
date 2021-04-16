6ee38b7ddaf8bcc4d2c1e84e9e3c11bb96b142d3
avibsoa <avibsoa.github@gmail.com>
2021-04-16 09:16:57 +0530

    Full pipline tested

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Avinash , updating from windows using git:'));
+app.get('/', (req, res) => res.send('AWS CI/CD completed ...!!! with GIT, AWS code Build & ELasticBeanStalk:'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
